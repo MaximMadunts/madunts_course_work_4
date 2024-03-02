f34fb6873a10507e7d3262bb5df799d9353e82d6
Maxim Madunts <max.madunts@gmail.com>
2024-03-02 20:30:42 +0300

    finish cw4

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from src.classes import HeadHunterAPI, Connector, Vacancy
+from tabulate import tabulate
+import pandas as pd
+import os
+import json
 
 
 def main():
-    global vacancies
-    vacancies_json = []
-    keyword = "Python"
+    # keyword = "Python"
+    keyword = input("Введите ключевое слово вакансии: ")
 
     """Создание экземпляров класса для работы с API сайта с вакансиями"""
-    hh = HeadHunterAPI(keyword)
-
-
-
-    # Сохранение информации о вакансиях в файл
-    connector = Connector(keyword=keyword)
-    connector.insert(vacancies_json=vacancies_json)
-    connector.select(vacancies_json=vacancies_json)
+    hh = HeadHunterAPI()
+    path = 'data/vacancies.json'
+    if os.path.exists(path):
+        with open(path, 'r', encoding='utf-8') as json_file:
+            vacs = json.load(json_file)
+            connector = Connector(keyword=keyword, vacancies_list=vacs)
+    else:
+        vacs = hh.get_vacancies()
+        connector = Connector(keyword=keyword, vacancies_list=vacs)
+        connector.insert()
 
     while True:
         command = input(
@@ -23,16 +28,20 @@ def main():
             "exit - для выхода.\n"
             ">>> "
         )
+        print(command)
         if command.lower() == "exit":
             break
-        elif command == "1":
-            vacancies = connector.select("Python")
-        elif command == "2":
-            vacancies = connector.sorted_by_salary()
-        for vacancy in vacancies:
-            print(vacancy, end="\n")
+        elif command == '1':
+            vacancies = connector.select()
+            df = pd.DataFrame(vacancies)
+            df = tabulate(df, headers=['Компания', 'Должность', 'Ссылка на вакансию', 'ЗП от', 'ЗП до', 'Валюта',
+                                       'Город'])
+            print(df)
+        elif command == '2':
+            connector.sorted_by_salary()
+
+
 
 
 if __name__ == "__main__":
     main()
-
--- src/classes.py
+++ src/classes.py
@@ -1,14 +1,36 @@
 import json
 import os
+import random
+import time
 from abc import ABC, abstractmethod
 from configparser import ParsingError
-from fileinput import filename
 
 import requests
+from pydash import get
+
+
+class ArcConnector(ABC):
+    """Абстрактный родительский класс"""
+
+    @abstractmethod
+    def insert(self):
+        pass
+
+    @abstractmethod
+    def select(self):
+        pass
+
+    @abstractmethod
+    def sorted_by_salary(self):
+        pass
+
+    @abstractmethod
+    def delete(self):
+        pass
 
 
 class Engine(ABC):
-    '''Абстрактный родительский класс'''
+    """Абстрактный родительский класс"""
 
     @abstractmethod
     def get_request(self):
@@ -16,113 +38,109 @@ class Engine(ABC):
 
     @abstractmethod
     def get_formatted_vacancies(self):
-        pass
-    @abstractmethod
-    def get_vacancies(self):
         pass
 
 
 class HeadHunterAPI(Engine):
-    url = "https://api.hh.ru/vacancies/"
 
-    def __init__(self, keyword):
+    def __init__(self):
 
-        self.params = {
-            "per_page": 100,
-            "page": int,
-            "text": keyword,
-            "archived": False,
+        self.headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Cookie': 'hhtoken=SgrlbHpPwXasO17_B38Vm1tgyUGt; hhuid=q9OKf2FoOGIzXmPD0KgyCw--; _ga=GA1.2.466181570.1673777324; _ga_44H5WGZ123=GS1.1.d552011a6f446deffc60438eda4e1d6486fbc35788baa01ae0bcc39182ddec7b.46.1.1678434110.60.0.0; hhul=e962eaedfb678fa1d5c142826c3ef21d5de6472513210c9a1d0840838751817f; cookie_policy_agreement=true; __zzatgib-w-hh=MDA0dC0jViV+FmELHw4/aQsbSl1pCENQGC9LX3svOx8gaEoVJ3gPTX5bTxY1JCkNCRRiQnYpeDA+Z1AZOVURCxIXRF5cVWl1FRpLSiVueCplJS0xViR8SylEXE18Jx0TfG8nVQ4RVy8NPjteLW8PKhMjZHYhP04hC00+KlwVNk0mbjN3RhsJHlksfEspNRJ+CixOQjFzJwl/DhlDSHF4XEIhHhl5WSZLDn8JKk5CeHQrCQwLGUYzaWVpcC9gIBIlEU1HGEVkW0I2KBVLcU8cenZffSpCZSBhS1skR1lTCCYVe0M8YwxxFU11cjgzGxBhDyMOGFgJDA0yaFF7CT4VHThHKHIzd2UtPmUjaEhcJDVRP0FaW1Q4NmdBEXUmCQg3LGBwVxlRExpceEdXeiwZFHlvJVQLD2FERWllbQwtUlFRS2IPHxo0aQteTA==a2Nhhg==; gsscgib-w-hh=VsOXs5CDk0cZAEmLr9aJQkM9gagskovvMWTXzBmJSHJvDthf+ojuyurpA/EowdpFfyXV9OgYfA/mFUmuGGJBETVxxx+6g4Vk/auN/S3DCoTaOB00ywwpUnEsbIZ5PkiFopVL66t7tQ8p3BBDxw0KvL5Wo3T5fACMVa90FOvVqwidil2hUv5Un/XthXcG1QUA4nfaqTUMjY62CjVSgMgYPbwSZUU+90C1u/I3jXUsemzKTVXNYCq2j01+v0Vkx7WBiq0=; fgsscgib-w-hh=KMsp09dd9bdb5ca38cdba5b2f3c24b01433547f7; cfidsgib-w-hh=GFrmR8FvoNOVBx7sTMpOFrHzWW0O8yCVzVEsSaH5XXLI3/pua+rxOhNnKSBykdMLF1hXWIK5UB3oT/lUCUtD5M92QqQULWrYtgYO1QzK4WupDGcqeoC+/dV+XgB+NQy8q5+VdKF4Ila7Hu1ZQB5ey/2JYvjKXIrfOpwivYZq; __ddg1_=smIJLZa24J0BGnDToMrZ',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
         }
-        self.vacancies = []
 
-    def get_request(self):
-        response = requests.get(self.url, params=self.params)
+    def get_request(self, url):
+        response = requests.get(url, headers=self.headers)
         if response.status_code != 200:
-            raise ParsingError(f'Ошибка полчения вакасний! Статус: {response.status_code}')
-        return response.json()
+            raise ParsingError(f'Ошибка получения вакансий! Статус: {response.status_code}')
+        res = response.json()
+        return res['items']
 
-    def get_formatted_vacancies(self):
+    def get_formatted_vacancies(self, url):
         formatted_vacancies = []
-        for vacancy in self.vacancies:
+        for vacancy in self.get_request(url):
             formatted_vacancy = {
-                "employer": vacancy["employer"]["name"],
-                "title": vacancy["area"]["name"],
-                "url": vacancy["area"]["url"],
-                "salary_from": vacancy["salary"]["from"] if vacancy["salary"] else None,
-                "salary_to": vacancy["salary"]["to"] if vacancy["salary"] else None,
-                "currency": vacancy["salary"]["currency"]
-
+                "employer": get(vacancy, "employer.name"),
+                "title": get(vacancy, "name"),
+                "url": get(vacancy, "alternate_url"),
+                "salary_from": get(vacancy, "salary.from"),
+                "salary_to": get(vacancy, "salary.to"),
+                "currency": get(vacancy, "salary.currency"),
+                "country": get(vacancy, "area.name")
             }
             formatted_vacancies.append(formatted_vacancy)
         return formatted_vacancies
 
-    def get_vacancies(self, pages_count=2):
-        self.vacancies = []
-        for page in range(pages_count):
-            page_vacancies = []
-            self.params["page"] = page
+    def get_vacancies(self, pages_count=19):
+        vacancies = []
+        r = random.randint(1, 2)
+        time.sleep(r)
+        for page in range(1, pages_count):
             print(f"({self.__class__.__name__}) Парсинг страницы {page} -", end=" ")
+            url = f"https://api.hh.ru/vacancies/?per_page=100&page={page}"
             try:
-                page_vacancies = self.get_request()
+                page_vacancies = self.get_formatted_vacancies(url)
+                vacancies.extend(page_vacancies)
+                print(f"Загружено вакансий: {len(vacancies)}")
             except ParsingError as error:
                 print(error)
-            else:
-                self.vacancies.extend(page_vacancies["items"])
-                print(f"Загружено вакансий: {len(page_vacancies)}")
-            if len(page_vacancies) == 0:
-                break
-        return self.vacancies
+
+        return vacancies
 
 
 class Vacancy:
-
-    def __init__(self, employer, title, url, salary_from, salary_to):
+    def __init__(self, employer, title, url, salary_from, salary_to, currency, country):
         self.employer = employer  # работодатель
         self.title = title  # вакансия
         self.url = url
         self.salary_from = salary_from
         self.salary_to = salary_to
+        self.currency = currency
+        self.country = country
 
     def __str__(self):
+
         return f"Работодатель: {self.employer}\n" \
                f"Вакансия: {self.title}\n" \
                f"Зарплата: от {self.salary_from} до {self.salary_to}\n" \
-               f"Ссылка: {self.url}"
+               f"Ссылка: {self.url}\n" \
+               f"Город: {self.country}\n" \
+               f"Валюта: {self.currency}\n"
 
     def __ge__(self, other):
-        if self.salary_from and other.salary_from != None:
-            return self.salary_from >= other.salary_from
+        if self.salary_from is not None and other.salary_to is not None:
+            return self.salary_from >= other.salary_to
+        else:
+            print("ЗП не указана")
 
-    def __lt__(self, other):
-        if self.salary_from and other.salary_from != None:
-            return self.salary_from < other.salary_from
+    def __le__(self, other):
+        if self.salary_from is not None and other.salary_to is not None:
+            return self.salary_from <= other.salary_to
+        else:
+            print("ЗП не указана")
 
 
-class Connector:
+class Connector(ArcConnector):
+    def __init__(self, vacancies_list, keyword):
+        self.keyword = keyword
+        self.vacancies_list = vacancies_list
 
-    def __init__(self, keyword):
-        self.filename = f"{keyword.title()}.json"
+    def insert(self):
+        f = json.dumps(self.vacancies_list, ensure_ascii=False, indent=4)
+        with open('data/vacancies.json', 'w', encoding='utf-8') as json_file:
+            json_file.write(f)
 
-    def insert(self, vacancies_json):
-        self.vacancies_json = vacancies_json
-        """Записывает информацию в файл"""
-
-        with open(self.filename, "w", encoding="utf-8") as file:
-            json.dump(self.vacancies_json, file, indent=4, ensure_ascii=False)
-
-    def select(self, vacancies_json):
-        self.vacancies_json = vacancies_json
-        """Открывает для чтения информацию из файла"""
-
-        with open(self.filename, "r", encoding="utf-8") as file:
-            vacancies = json.load(file)
-        return [Vacancy(**x) for x in vacancies]
+    def select(self):
+        selected_vacs = []
+        for i in self.vacancies_list:
+            if self.keyword in i['title']:
+                selected_vacs.append(i)
+        return selected_vacs
 
     def sorted_by_salary(self):
-        self.vacancies = []
+        return [print(Vacancy(**x)) for x in self.select()]
 
-        with open("Python.json", "r", encoding="utf-8") as file:
-            vacancies = json.load(file)
-        vacancies = [Vacancy(**x) for x in vacancies]
-        vacancies.sort()
-        return self.vacancies
+    def delete(self):
+        os.remove(self.filename)
